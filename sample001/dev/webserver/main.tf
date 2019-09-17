2187ca0977177fb26fb387ffe39ebb1fd3e146d2
uchijima <uchijima@nextscape.net>
2019-09-17 19:57:28 +0900

    [add]application insightsを追加

--- sample001/dev/webserver/main.tf
+++ sample001/dev/webserver/main.tf
@@ -21,7 +21,21 @@ resource "azurerm_app_service" "test" {
   app_service_plan_id = "${azurerm_app_service_plan.test.id}"
 
   app_settings = {
-    "SOME_KEY" = "some-value"
+     APPINSIGHTS_INSTRUMENTATIONKEY = "${azurerm_application_insights.test.instrumentation_key}"
+		 APPINSIGHTS_PROFILERFEATURE_VERSION = "1.0.0"
+		 APPINSIGHTS_SNAPSHOTFEATURE_VERSION = "1.0.0"
+		 ApplicationInsightsAgent_EXTENSION_VERSION = "~2"
+		 DiagnosticServices_EXTENSION_VERSION = "~3"
+		 InstrumentationEngine_EXTENSION_VERSION = "~1"
+		 SnapshotDebugger_EXTENSION_VERSION = "~1"
+		 XDT_MicrosoftApplicationInsights_Mode = "recommended"
+		 XDT_MicrosoftApplicationInsights_BaseExtensions = "~1"
   }
 
+}
+resource "azurerm_application_insights" "test" {
+  name                = "tf-test-appinsights"
+  location            = "West Europe"
+  resource_group_name = "${azurerm_resource_group.test.name}"
+  application_type    = "web"
 }
